7e24e60987cd5b88057776072d9d19de533cabbd
maximkd971 <maxi97115@gmail.com>
2016-06-03 20:53:33 +0200

    ajoutEvt dans fichier

--- src/Modele/Frise.java
+++ src/Modele/Frise.java
@@ -1,6 +1,7 @@
 package Modele;
 
-import java.io.File;
+import javax.swing.JOptionPane;
+
 import java.io.*;
 
 
@@ -8,6 +9,15 @@ public class Frise {
 	String chTitre;
 	int chAnneeDebut;
 	int chAnneeFin;
+	Date chDate;
+	String chTitreEvt;
+	String chLieu;
+	String chImage;
+	String chDescription;
+	String nomFrise;
+	
+	
+	
 	public Frise (String titre, int anneeDebut, int anneeFin)
 	{
 		chTitre= titre;
@@ -15,6 +25,16 @@ public class Frise {
 		chAnneeFin = anneeFin;
 		creeFichier();
 		
+	}
+	public  Frise (Date parDate,String parTitre,String parLieu, String parImage ,String parDescription , String parFrise)
+	{
+		chDate = parDate;
+		chTitreEvt = parTitre;
+		chLieu = parLieu;
+		chImage = parImage;
+		chDescription = parDescription;
+		nomFrise= parFrise;
+		creeFichierEvt();
 	}
 	
 	public void creeFichier(){
@@ -27,16 +47,40 @@ public class Frise {
 			catch(IOException e){
 				e.printStackTrace();
 			}
-		}
+		} 
+		
+		
 		try (PrintWriter texte = new PrintWriter(new FileOutputStream(fichier,true)))
 		{
 			texte.println(chTitre);
 			texte.println(chAnneeDebut);
 			texte.println(chAnneeFin);
+			texte.println("");
 		}
 		catch(IOException e){
 			e.printStackTrace();
 			
 		}
+		
+	
+	}
+	public void creeFichierEvt(){
+		File fichier = new File("./FichierChrono/"+nomFrise);
+		try (PrintWriter texte = new PrintWriter(new FileOutputStream(fichier , true)))
+		{
+		
+			texte.println(chDate);
+			texte.println(chTitreEvt);
+			texte.println(chLieu);
+			texte.println(chImage);
+			texte.println(chDescription);
+			texte.println("");
+			
+		}
+		catch(IOException e){
+			e.printStackTrace();
+			
+		}
+		
 	}
 	}
